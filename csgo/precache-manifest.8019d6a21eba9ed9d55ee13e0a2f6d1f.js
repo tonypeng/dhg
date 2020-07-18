c75d73dd34856ad1d05dac9f62ec65ec0b43f689
Tony Peng <me@tonypeng.com>
2020-07-18 03:37:09 -0700

    import csgo site

---
+++ csgo/precache-manifest.8019d6a21eba9ed9d55ee13e0a2f6d1f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "966659e20de0a0a8ac7a40fd74702186",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0391de2c1d238aa8e3a3",
+    "url": "/static/css/main.22c4a0cf.chunk.css"
+  },
+  {
+    "revision": "8cf03d8406878a8e5af3",
+    "url": "/static/js/2.0e37751d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0e37751d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0391de2c1d238aa8e3a3",
+    "url": "/static/js/main.54e8b49f.chunk.js"
+  },
+  {
+    "revision": "c3a13f6311255a51b672",
+    "url": "/static/js/runtime-main.1ad2124c.js"
+  },
+  {
+    "revision": "3e594fe30daba397ee4c68398ac6de76",
+    "url": "/static/media/arjoon_logo.3e594fe3.png"
+  },
+  {
+    "revision": "a8ec7fe62ac7af3d9b74f708ab123c86",
+    "url": "/static/media/asstralis.a8ec7fe6.png"
+  },
+  {
+    "revision": "d2f91ca94f74f9b83ce2ad1897b2bd4b",
+    "url": "/static/media/csgo_logo.d2f91ca9.png"
+  },
+  {
+    "revision": "9d035360d0297f63d0f14b80b5f9fb9f",
+    "url": "/static/media/drooler_nation.9d035360.png"
+  },
+  {
+    "revision": "3a7c551cf87c5bb19332e6cbf7b63718",
+    "url": "/static/media/dust2_blurred.3a7c551c.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
