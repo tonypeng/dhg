73e75295f5b41198468d3d3863d16c79492f4d4a
Tony Peng <me@tonypeng.com>
2020-07-18 18:52:50 -0700

    update

---
+++ csgo/static/js/precache-manifest.5a95e96bce45544e9a201bf47837cba7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f2074d0e99287f7f3d23ac6809090b",
+    "url": "/csgo/index.html"
+  },
+  {
+    "revision": "a9e82ba782258a85e281",
+    "url": "/csgo/static/css/main.9372fbf7.chunk.css"
+  },
+  {
+    "revision": "56759cebfe306fe8a49f",
+    "url": "/csgo/static/js/2.d8221de8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/csgo/static/js/2.d8221de8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9e82ba782258a85e281",
+    "url": "/csgo/static/js/main.3dd44b68.chunk.js"
+  },
+  {
+    "revision": "e87e42405c97c63edba9",
+    "url": "/csgo/static/js/runtime-main.eb4a7917.js"
+  },
+  {
+    "revision": "3e594fe30daba397ee4c68398ac6de76",
+    "url": "/csgo/static/media/arjoon_logo.3e594fe3.png"
+  },
+  {
+    "revision": "0682ae907ce1726acf56b27e0c9bfc9e",
+    "url": "/csgo/static/media/asstralis.0682ae90.png"
+  },
+  {
+    "revision": "d2f91ca94f74f9b83ce2ad1897b2bd4b",
+    "url": "/csgo/static/media/csgo_logo.d2f91ca9.png"
+  },
+  {
+    "revision": "9d035360d0297f63d0f14b80b5f9fb9f",
+    "url": "/csgo/static/media/drooler_nation.9d035360.png"
+  },
+  {
+    "revision": "3a7c551cf87c5bb19332e6cbf7b63718",
+    "url": "/csgo/static/media/dust2_blurred.3a7c551c.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/csgo/static/media/logo.5d5d9eef.svg"
+  }
+]);
